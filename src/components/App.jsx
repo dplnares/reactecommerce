7c013b284ba61c83ed8bb831970f71a6816e05b6
David Poblette <dpoblettelinares@gmail.com>
2023-12-02 20:40:32 -0500

    Estructura de carpetas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 
-import {API_URL} from "../env"
+import {API_URL} from "../constants/env"
 
 function App() {
 
   return (
     <>
       <h1>Hola mundo {API_URL}</h1>
+      <p>Este es el documento</p>
     </>
   )
 }
